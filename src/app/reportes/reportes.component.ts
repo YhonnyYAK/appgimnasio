58ae087c47eb11d0b91e953d2ecae365c51144b9
Yoni Abad <yoniabad@gmail.com>
2020-11-02 14:18:16 -0500

    Botton de Descargar PDF Clientes

--- src/app/reportes/reportes.component.ts
+++ src/app/reportes/reportes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas'; // Todavía no lo usamos
 
 @Component({
   selector: 'app-reportes',
@@ -8,7 +10,7 @@ import { AngularFirestore } from '@angular/fire/firestore';
 })
 export class ReportesComponent implements OnInit {
   clientes: any[] = new Array<any>();
-  constructor(private db: AngularFirestore) { }
+  constructor(private db: AngularFirestore) {   }
 
   ngOnInit(): void {
 
@@ -23,6 +25,31 @@ export class ReportesComponent implements OnInit {
         cliente.ref = item.ref;
         this.clientes.push(cliente);
       });
+    });
+  }
+
+  public downloadPDF(): void {
+
+    const DATA = document.getElementById('htmlData');
+    const doc = new jsPDF('p', 'pt', 'a4');    
+    const options = {
+      background: 'white',
+      scale: 3
+    };
+    html2canvas(DATA, options).then((canvas) => {
+
+      const img = canvas.toDataURL('image/PNG');
+
+      // Add image Canvas to PDF
+      const bufferX = 15;
+      const bufferY = 15;
+      const imgProps = (doc as any).getImageProperties(img);
+      const pdfWidth = doc.internal.pageSize.getWidth() - 2 * bufferX;
+      const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+      doc.addImage(img, 'PNG', bufferX, bufferY, pdfWidth, pdfHeight, undefined, 'FAST');
+      return doc;
+    }).then((docResult) => {
+      docResult.save(`${new Date().toISOString()}_Clientes.pdf`);
     });
   }
 
