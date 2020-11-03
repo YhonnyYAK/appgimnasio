b3ea93987f95ee996c531bbb7630c26cae3d9716
Yoni Abad <yoniabad@gmail.com>
2020-11-03 02:34:10 -0500

    Botton de Descargar EXCEL Clientes

--- src/app/reportes/reportes.component.ts
+++ src/app/reportes/reportes.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import jsPDF from 'jspdf';
 import html2canvas from 'html2canvas'; // Todavía no lo usamos
+import * as FileSaver from 'file-saver';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-reportes',
@@ -51,6 +53,26 @@ export class ReportesComponent implements OnInit {
     }).then((docResult) => {
       docResult.save(`${new Date().toISOString()}_Clientes.pdf`);
     });
+  }
+
+  fileType = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+  fileExtension = '.xlsx';
+
+  public exportExcel(jsonData: any[], fileName: string): void {
+
+    const ws: XLSX.WorkSheet = XLSX.utils.json_to_sheet(jsonData);
+    const wb: XLSX.WorkBook = { Sheets: { 'data': ws }, SheetNames: ['data'] };
+    const excelBuffer: any = XLSX.write(wb, { bookType: 'xlsx', type: 'array' });
+    this.saveExcelFile(excelBuffer, fileName);
+  }
+
+  private saveExcelFile(buffer: any, fileName: string): void {
+    const data: Blob = new Blob([buffer], {type: this.fileType});
+    FileSaver.saveAs(data, fileName + this.fileExtension);
+  }
+
+  export() {
+    this.exportExcel(this.clientes, 'clientes');
   }
 
 }
